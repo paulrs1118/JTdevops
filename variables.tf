d36746df7bac2e1364af9febe9f58a9ba3af7630
paulrs1118 <paulrs1118@gmail.com>
2023-03-09 22:40:50 +0000

    Adding variables.tf file

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-03d315ad33b9d49c4"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.micro"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     =  "jenkins-terraform"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "jtdevops-sg"
+}
+
+variable "jtdevops_subnet"{
+  description = "Name of the subnet"
+  default     = "jtdevops-subnet"
+}
+
