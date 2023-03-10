d0939a409510c4b179efdafa58e4930179c65b0e
paulrs1118 <paulrs1118@gmail.com>
2023-03-10 16:57:05 +0000

    Modifying instance.tf file

--- instance.tf
+++ instance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "jtdevops-ubuntu-instance" {
   ami                         = "ami-0557a15b87f6559cf"
   instance_type               = "t2.micro"
-  key_name                    = "jenkins-terraform"
-  subnet_id                   = sws_subnet.ubnet-06e699f4f5ad9f4d3.id
+  key_name                    = aws_key_pair.jenkins-terraform.key_name
+  subnet_id                   = aws_subnet.ubnet-06e699f4f5ad9f4d3.id
   associate_public_ip_address = true
 
   tags = {
